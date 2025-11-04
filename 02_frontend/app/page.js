a2cf29b4384cc74d77d37fa598b64bb819197b21
MYAMADDY <myamaddy.k67@rsu.ac.th>
2025-11-04 18:06:00 +0700

    Add my name

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">6708204-Mya Maddy Ko Ko</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
